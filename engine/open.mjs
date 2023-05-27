9ea5d6ee9eb56f3cc36bee34331b3d7bd36e7588
markjuno <mark@the-juno.com>
2023-05-27 22:59:44 +0300

    open an html pipe

---
+++ engine/open.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import * as fs from 'fs';
+import open from 'open';
+(async function () {
+  // Read input from command line arguments or pipe
+  let input = '';
+  if (process.argv.length > 2) {
+    input = process.argv.slice(2).join(' ');
+  } else {
+    input = await new Promise((resolve) => {
+      let data = '';
+      process.stdin.on('data', (chunk) => {
+        data += chunk;
+      });
+      process.stdin.on('end', () => {
+        resolve(data);
+      });
+    });
+  }
+
+  try {
+    fs.writeFileSync('./../temp/page.html', input, (err) => {
+      if (err) {
+        console.error('Error writing HTML file:', err);
+        return;
+      }
+    });  
+    open('./../temp/page.html');
+  } catch (error) {
+    console.error('Error occurred during code execution:', error);
+  }
+})();
